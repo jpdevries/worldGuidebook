d46cee1dfe34a504e7d8212180ad5cc603b47b80
JP DeVries <mail@devries.jp>
2016-07-30 20:39:29 +0200

    Adding grunt-copy
    now when you run build files will be fetched from bower and copied into
    place. This is handy because your assets will auto update and be moved
    into place

--- gruntfile.js
+++ gruntfile.js
@@ -1,12 +1,56 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        dirs:{
+          build:'_build/',
+          theme:'./',
+          lib:'./lib/',
+          src:'src/',
+          assets:'assets/'
+        },
+        bower: {
+            install: {
+                options: {
+                    targetDir: '<%=dirs.theme %><%=dirs.lib %>',
+                    layout: 'byComponent'
+                }
+            }
+        },
+        copy:{
+          angular:{
+            files:[{
+              src: 'angular/**/*',
+              cwd: '<%= dirs.lib %>',
+              dest: '<%= dirs.theme %><%= dirs.assets %>',
+              expand:true,
+              flatten:true
+            }]
+          },
+          angularAnimate:{
+            files:[{
+              src: 'angular-animate/**/*',
+              cwd: '<%= dirs.lib %>',
+              dest: '<%= dirs.theme %><%= dirs.assets %>',
+              expand:true,
+              flatten:true
+            }]
+          },
+          angularRoute:{
+            files:[{
+              src: 'angular-route/**/*',
+              cwd: '<%= dirs.lib %>',
+              dest: '<%= dirs.theme %><%= dirs.assets %>',
+              expand:true,
+              flatten:true
+            }]
+          }
+        },
         concat: {
             options: {
                 separator: ';',
             },
             js: {
-                src: ['src/*.js'], 
+                src: ['src/*.js'],
                 dest: 'dist/concat.js', //Change to where you want your pretty frankenfile to land
             },
             css: {
@@ -43,11 +87,13 @@ module.exports = function(grunt) {
             }
         }
     });
-    
+
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    
-    grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'watch']);
+
+    grunt.registerTask('default', ['bower','copy','concat', 'uglify', 'cssmin', 'watch']);
 };
